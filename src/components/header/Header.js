87d882b3b52b39bb7f2a61528a7e6eec2bf37b85
rony <rony49883@gmail.com>
2021-11-08 12:30:47 +0600

    Add a simple header section under the menubar

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className = "header_Section">
+            <h1>Dhaka Ambulance Service</h1>
+            <h4>Let's help you to find your needable ambulance in easier and simple way.</h4>
+            <h4>Here We Have Almost <span><i> 1000 </i></span>Ambulances</h4>
+        </div>
+    );
+};
+
+export default Header;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from '../navBar/NavBar';
 import Footer from '../footer/Footer';
+import Header from '../header/Header';
 
 const Home = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <Header></Header>
 
             <Footer></Footer>
             
